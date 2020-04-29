e93f198dff2f5f030ef4ed547d312e28e2e236f3
Moarqi <mrothgaenger@techfak.uni-bielefeld.de>
2020-04-30 01:02:59 +0200

    added simple imgui window

--- src/Spheremarcher.cpp
+++ src/Spheremarcher.cpp
@@ -28,6 +28,21 @@ void Spheremarcher::initialize()
     marchingShader_.SetUniform("ULightDirection", glm::normalize(glm::vec3(-1.0f, -1.0f, 0.5f)));
     marchingShader_.SetUniform("UScene", scene_);
     marchingShader_.Unbind();
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsClassic();
+
+    // Setup Platform/Renderer bindings
+    ImGui_ImplGlfw_InitForOpenGL(window_, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
 }
 
 void Spheremarcher::resize(int width, int height)
@@ -36,6 +51,26 @@ void Spheremarcher::resize(int width, int height)
 
 void Spheremarcher::draw()
 {
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    {
+        static float f = 0.0f;
+        static int counter = 0;
+
+        ImGui::Begin("debug"); // Create a window called "Hello, world!" and append into it.
+
+        ImGui::SliderFloat3("sphere position", &scene_.Spheres[0].position[0], -10.0f, 10.0f); // Edit 1 float using a slider from 0.0f to 1.0f
+        // ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+        ImGui::SameLine();
+        ImGui::Text("counter = %d", counter);
+
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        ImGui::End();
+    }
+    ImGui::Render();
     // TODO: create VertexArray class
     unsigned int vao, ibo;
     glGenVertexArrays(1, &vao);
@@ -47,8 +82,10 @@ void Spheremarcher::draw()
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, 3 * sizeof(unsigned int), nullptr, GL_STATIC_DRAW);
 
     marchingShader_.Bind();
+    marchingShader_.SetUniform("UScene", scene_);
     // this enables to draw the screen filling triangle in the vertex shader
     glDrawArrays(GL_TRIANGLES, 0, 3);
-
     marchingShader_.Unbind();
+
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
